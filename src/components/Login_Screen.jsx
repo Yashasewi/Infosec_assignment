87298f0bfeeb5ed9b51cf7349827fc67aecd99b5
Yashasewi <Yashasewisingh2668@gmail.com>
2023-08-12 23:41:10 +0530

    login page done

--- src/components/Login_Screen.jsx
+++ src/components/Login_Screen.jsx
@@ -1,3 +1,23 @@
 export default function Login_Screen() {
-    return <div className="login">Login_Screen</div>;
+    return (
+        <div className="login">
+            <h1>Signin to your</h1>
+            <h1>PopX Account</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                aliquam. Culpa minima
+            </p>
+            <form>
+                <span>
+                    <label className="email">Email Address</label>
+                    <input type="email" placeholder="Email Address" />
+                </span>
+                <span>
+                    <label className="password">Password</label>
+                    <input type="password" placeholder="Password" />
+                </span>
+                <button>Login</button>
+            </form>
+        </div>
+    );
 }
--- src/components/SignIn_Screen.jsx
+++ src/components/SignIn_Screen.jsx
@@ -1,24 +1,50 @@
 export default function SignIn_Screen() {
     return (
         <div className="signIn">
-            <h1>Signin to your</h1>
+            <h1>Create your</h1>
             <h1>PopX Account</h1>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                aliquam. Culpa minima
-            </p>
+
+            {/* create a form with full name , phone number 
+            , email addrees  , password , company name and 
+            radio button of agency yes and no */}
             <form>
                 <span>
-                    <label
-                        className="email"
-                    >Email Address</label>
+                    <label className="fullName">Full Name</label>
+                    <input type="text" placeholder="Full Name" />
+                </span>
+
+                <span>
+                    <label className="phoneNumber">Phone Number</label>
+                    <input type="text" placeholder="Phone Number" />
+                </span>
+
+                <span>
+                    <label className="email">Email Address</label>
                     <input type="email" placeholder="Email Address" />
                 </span>
                 <span>
                     <label className="password">Password</label>
                     <input type="password" placeholder="Password" />
                 </span>
-                <button>Login</button>
+                <span>
+                    <label className="companyName">Company Name</label>
+                    <input type="text" placeholder="Company Name" />
+                </span>
+                <span>
+                    <label className="agency">Are you an agency?</label>
+                    <span className="radio_btn">
+                        <span>
+                            <input type="radio" name="agency" value="yes" />
+                            <label>Yes</label>
+                        </span>
+                        <span>
+                            <input type="radio" name="agency" value="no" />
+                            <label>No</label>
+                        </span>
+                    </span>
+                </span>
+
+                <button>Create Account</button>
             </form>
         </div>
     );
