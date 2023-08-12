9b1450d618aceb8db3d31d7c31720b49912dcbe3
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-08-13 00:26:11 +0530

    profile page done

--- src/components/Profile_Screen.jsx
+++ src/components/Profile_Screen.jsx
@@ -1,3 +1,24 @@
 export default function Profile_Screen() {
-    return <div className="profile">Profile_Screen</div>;
+    return (
+        <div className="profile">
+            <h1>Account Settings</h1>
+            <div className="profile__details">
+                <img
+                    className="profile_image"
+                    src="https://picsum.photos/80/80"
+                    alt="profile"
+                />
+                <div className="userData">
+                    <h2>Marry Doe</h2>
+                    <h3>Marry@Gmail.com</h3>
+                </div>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deserunt porro pariatur minima doloremque suscipit natus est
+                voluptatibus nisi quo sed facere fugit, sequi ullam.
+            </p>
+            <div className="dashed"></div>
+        </div>
+    );
 }
