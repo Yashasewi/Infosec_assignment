7760938885f34aab59251c83ef568a7138917c52
Yashasewi <Yashasewisingh2668@gmail.com>
2023-08-13 01:34:53 +0530

    css styling change and bug fixing and routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import "./App.css";
-import { useState } from "react";
-import SignIn_Screen from "./Pages/SignIn/SignIn_Screen";
-import Login_Screen from "./Pages/Login/Login_Screen";
-import Profile_Screen from "./Pages/Profile/Profile_Screen";
-import Landing_Screen from "./Pages/Landing/Landing_Screen";
-export default function App() {
-    const [Login, setLogin] = useState(false);
-    const [CreateAccount, setCreateAccount] = useState(false);
 
+import Landing_Screen from "./Pages/Landing/Landing_Screen";
+
+export default function App() {
     return (
         <div className="app">
-            <Landing_Screen
-                setLogin={setLogin}
-                setCreateAccount={setCreateAccount}
-            />
-            <SignIn_Screen
-                setLogin={setLogin}
-                setCreateAccount={setCreateAccount}
-            />
-            <Login_Screen
-                setLogin={setLogin}
-                setCreateAccount={setCreateAccount}
-            />
-            <Profile_Screen
-                setLogin={setLogin}
-                setCreateAccount={setCreateAccount}
-            />
+            <Landing_Screen />
         </div>
     );
 }
--- src/Pages/Landing/Landing_Screen.jsx
+++ src/Pages/Landing/Landing_Screen.jsx
@@ -1,30 +1,19 @@
 import "./landing.css";
+import { Link } from "react-router-dom";
 
-export default function Landing_Screen({ setLogin, setCreateAccount }) {
-    const handleLoginClick = () => {
-        setLogin(true);
-        setCreateAccount(false);
-    };
-
-    const handleCreateAccountClick = () => {
-        setLogin(false);
-        setCreateAccount(true);
-    };
+export default function LandingScreen() {
     return (
-        <div className="landing_screen">
+        <div className="landing-screen">
             <h1>Welcome to PopX</h1>
             <p>
                 PopX is a web application that allows you to search for movies
                 and TV shows.
             </p>
-            <button className="create_account" onClick={handleLoginClick}>
-                Create Account
+            <button className="landing-button create-account">
+                <Link to="/SignIn">Create Account</Link>
             </button>
-            <button
-                className="login_account"
-                onClick={handleCreateAccountClick}
-            >
-                Already Registered? Login
+            <button className="landing-button login-account">
+                <Link to="/Login">Already Registered? Login</Link>
             </button>
         </div>
     );
